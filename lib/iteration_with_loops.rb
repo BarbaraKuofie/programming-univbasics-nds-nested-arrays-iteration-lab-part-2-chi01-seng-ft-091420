67b0644699a539d73489297004da24c2056c52fd
Barbara Kuofie <barbara.kuofie@gmail.com>
2020-08-29 04:05:38 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,8 +6,10 @@ row_index = 0
 while row_index < src.count do 
   element_index = 0 
   while element_index < src[row_index].count do
-    if src[row_index][element_index]
-  count = 0 
-end 
-count += 1 
+    if src[row_index][element_index].min 
+      cold_days << src[row_index][element_index]
+    end 
+    element_index += 1 
+ end 
+row_index += 1 
 end
